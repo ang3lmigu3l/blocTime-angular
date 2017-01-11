2c47af73246d2c302f9d6f736940cd703a5e8130
Miguel Angel Quintana <quintana.mige@gmail.com>
2017-01-10 22:21:44 -0800

    created myButton.js directive file. completed checkpoint 3

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,52 +12,9 @@
         });
         
     };
-    function myButton($interval) {
-        return {
-            templateUrl: '/templates/myButton.html',
-            restrict: 'E',
-            replace: true,
-            scope: { },
-            link: function(scope, element, attributes) {
-                scope.workTime = 1500;
-                scope.buttonText = "START";
-                
-                var timeSet;
-                
-                scope.countdown = function() {
-
-                   if(scope.workTime === 0)
-                   {
-                       $interval.cancel(timeSet);
-                       scope.workTime = 1500;
-                       scope.buttonText = "START";
-                   }
-                   else
-                   {
-                    scope.workTime--;
-                   }
-                }
-                scope.startTimer = function() {
-                     if(scope.buttonText == "RESET") {
-                         scope.workTime = 1500;
-                         $interval.cancel(timeSet);
-                         scope.buttonText = "START";
-                         console.log( "test restarted");
-
-                     } else {
-                         timeSet = $interval(scope.countdown,1000);
-                         scope.buttonText = "RESET";
-                         console.log("test started");
-                     }
-                }
-            }
-        }
-
-    };
-    
+            
     
     angular
         .module('blocTime', ['ui.router', 'firebase'])
-        .config(config)
-        .directive('myButton',['$interval', myButton]);
+        .config(config);
 })();
--- dist/scripts/directives/myButton.js
+++ dist/scripts/directives/myButton.js
@@ -1,24 +1,81 @@
-//(function() {
-//    function myButton($interval) {
-//        return {
-//            templateUrl: '/templates/myButton.html',
-//            restrict: 'E',
-//            replace: true,
-//            scope: { },
-//            link: function(scope, element, attributes) {
-//                scope.workTime = 1500;
-//                scope.buttonText = "START";
-//                scope.startTimer = function {
-//                    if (scope.buttonText == "RESET") {
-//                        
-//                    }
-//                }
-//            };
-//        }
-//            
-//    }
-//
-//    angular
-//        .module('blocTime')
-//        .directive('myButton', ['$interval', myButton]);
-//})();
+(function() {
+    function myButton($interval, MY_TIMES) {
+        return {
+            templateUrl: '/templates/myButton.html',
+            restrict: 'E',
+            replace: true,
+            scope: { },
+            link: function(scope, element, attributes) {
+                //sets first timer to 25m . 
+                scope.workTime = MY_TIMES.work;
+                // sets default button as Start 
+                scope.buttonText = "START";
+                // 
+                scope.onBreakreak = false; 
+                // starts countdown from current work/break time 
+                var timeSet;
+                
+                                
+                var setBreak =  function() {
+                    $interval.cancel(timeSet);
+                    scope.workTime = MY_TIMES.break; 
+                    scope.buttonText = "START";
+                    scope.onBreak = true;                   
+                };
+                
+                var setWork = function() {
+                    $interval.cancel(timeSet);
+                    scope.workTime = MY_TIMES.work;
+                    scope.buttonText = "START";   
+                    scope.onBreak = false;
+                    
+                };
+                
+                scope.countdown = function() {
+                   if(scope.workTime <= 0){
+                       //if countdown reaches 0  and is on break , set time to 25m (work) 
+                           if (scope.onBreak) {
+                               setWork();
+                               console.log("currently working");
+                        // else set timer for 5m  and starts break timer.
+                           } else {
+                               setBreak();
+                               console.log('currently on break');
+                           };
+                   }else{
+                    //countdown 
+                    scope.workTime--;
+                    }
+                };
+                
+                           
+                scope.toggleTimer = function() {
+                     if(scope.buttonText == "RESET") {
+                         if (scope.onBreak) {
+                             setBreak();
+                             console.log("restart break Timer")
+                                
+                         } else {
+                             setWork();
+                             console.log( "restarted work timer");
+                         };
+
+                     } else {
+                         timeSet = $interval(scope.countdown,1000);
+                         scope.buttonText = "RESET";
+                         console.log("start countdown");
+                     };
+                };
+            }
+        }  
+
+    };
+
+    angular
+        .module('blocTime')
+        .directive('myButton',['$interval', 'MY_TIMES', myButton])
+        .constant("MY_TIMES", {
+            "work": 5,
+            "break": 3
+        });
+})();
